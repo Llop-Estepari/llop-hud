ae813eca06d5e353064b6c842a46eda77116181f
David <blackhear12@gmail.com>
2024-03-12 13:51:11 +0100

    Test

--- config.lua
+++ config.lua
@@ -2,6 +2,8 @@ Config = {}
 
 
 Config.SpeedometerUpdateRate = 100 --The update rate of the speedometer in milliseconds, lower values = faster updates.(Recommended: min 100, max 300).
+Config.SpeedometerInCenter = false
+
 Config.UseMPH = false --Enbale or disable the use of MPH
 Config.FuelScript = '' --The script name of the your fuel script.
 --'legacyfuel', 'ox-fuel', 'ps-fuel'. List of compatible scripts.
